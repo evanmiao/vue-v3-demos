51bb162dc6dc762e047346d8fbe7524ea2891008
Evan Miao <evan.c.miao@gmail.com>
2020-09-09 10:52:47 +0800

    setup and ref

---
+++ src/views/Ref.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>count: {{ count }}</h1>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // ref() 创建响应式数据
+    const count = ref(0)
+
+    const add = () => {
+      // 注意：通过 ref() 封装的数据必须通过 .value 才能访问到里面的值
+      count.value++
+    }
+
+    return { count, add }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Setup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  // setup 函数是一个新的组件选项。作为在组件内使用 Composition API 的入口点。
+  setup() {
+    const msg = 'Hello, Vue 3!'
+    // setup 最后返回所有的对象和方法，注意这里的 msg 不是响应式对象
+    return { msg }
+  }
+}
+</script>
+
+<style>
+
+</style>
